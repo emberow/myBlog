24703705127dd88fb5f1eed67692dd0819138027
emberow <chin45630@gmail.com>
2023-10-28 10:54:28 +0800

    feat: add jwt error handle

--- backend/src/middleware/errors.ts
+++ backend/src/middleware/errors.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
 
 export interface CustomErrorContent {
   message: string,
@@ -24,11 +25,14 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
   if(err instanceof CustomError) {
     const { statusCode, errors } = err;
     return res.status(statusCode).send({ message: errors.message });
+  } else if (err instanceof TokenExpiredError) {
+    return res.status(403).send({ message: "TOKEN_EXPIRED" }); 
+  } else if (err instanceof JsonWebTokenError) {
+    return res.status(401).send({ message: "TOKEN_INVALID" });  
   }
 
   console.log(err);
   // Unhandled errors
-  console.error(JSON.stringify(err, null, 2));
   return res.status(500).send({ message: "Something went wrong" });
 };
 
