aab2846a350e01a57c33edebee6fc0545d20e9ce
emberow <chin45630@gmail.com>
2024-04-21 16:35:24 +0800

    fix: variable err

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,25 +2,25 @@ import axios from "axios";
 
 
 export const PostRequest = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
   timeout: 20000, // 延迟时间
   method: "POST",
 });
 
 export const GetRequest = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
   timeout: 20000, // 延迟时间
   method: "GET",
 });
 
 export const DeleteRequest = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
   timeout: 20000, // 延迟时间
   method: "DELETE",
 });
 
 export const PatchRequest = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
   timeout: 20000, // 延迟时间
   method: "PATCH",
 });
