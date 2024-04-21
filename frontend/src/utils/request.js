073f84f3ea5ee6fb8eb6f5474ac27a145066ffd5
emberow <chin45630@gmail.com>
2024-04-21 15:28:36 +0800

    fix: Let system variables take precedence over .env

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,28 +1,26 @@
 import axios from "axios";
 
-let baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
-
 
 export const PostRequest = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   timeout: 20000, // 延迟时间
   method: "POST",
 });
 
 export const GetRequest = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   timeout: 20000, // 延迟时间
   method: "GET",
 });
 
 export const DeleteRequest = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   timeout: 20000, // 延迟时间
   method: "DELETE",
 });
 
 export const PatchRequest = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   timeout: 20000, // 延迟时间
   method: "PATCH",
 });
